f846e7f4443a0082622ef8c1503f8ca94055c28d
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-10-12 23:51:59 +0530

    errorsolve

--- src/pages/educator/AddCourse.jsx
+++ src/pages/educator/AddCourse.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const AddCourse = () => {
   return (
-    <div>
-        <h1>AddCourse</h1>
-    </div>
+    <div>AddCourse</div>
   )
 }
 
--- src/pages/educator/educator.jsx
+++ src/pages/educator/educator.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const educator = () => {
   return (
     <div>
         <h1>educator</h1>
+        <div>
+          {<Outlet/>}
+        </div>
     </div>
   )
 }
