214ae25a3b1c01eb2655b8bd3411767b0f3b9652
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-12-24 00:39:50 +0530

    define

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,16 +1,28 @@
 import React, { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
     const currency = import.meta.env.VITE_CURRENCY;
+    const navigate = useNavigate();
+
 
     const [allCourses, setAllCourses] = useState([]);
 
     const fetchAllCourses = async () => {
         setAllCourses(dummyCourses);
     };
+
+    // Function to calculate average rating of a course
+    const calculateRating = (course) => {
+        const ratings = course?.courseRatings ?? [];
+        if (ratings.length === 0) return 0;
+        const total = ratings.reduce((sum, r) => sum + (r?.rating ?? 0), 0);
+        return total / ratings.length;
+    }
+
 
     useEffect(() => {
         fetchAllCourses();
@@ -19,8 +31,10 @@ export const AppContextProvider = ({ children }) => {
     const value = {
         currency,
         allCourses,
+        navigate,
         setAllCourses,
         fetchAllCourses,
+        calculateRating 
     };
 
     return (
