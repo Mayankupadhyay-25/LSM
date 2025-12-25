fd3e2117de84c4088e151b5a59d097d79bec4787
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-12-26 01:50:18 +0530

    dubbleTimeIssue

--- src/Components/student/Navbar.jsx
+++ src/Components/student/Navbar.jsx
@@ -5,53 +5,45 @@ import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 import { AppContext } from "../../context/AppContext";
 
 const Navbar = () => {
-  
-  const {navigate} = useContext(AppContext)
-
+  const {navigate, isEducator, setIsEducator} = useContext(AppContext)
   const isCourseListPage = location.pathname.includes("/Course-list");
-
   const { openSignIn } = useClerk();
   const { user } = useUser();
-
   return (
     <div
       className={`flex w-full items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${
         isCourseListPage ? "bg-white" : "bg-cyan-100/70"
-      }`}
-    >
+      }`}>
       <img onClick={()=> navigate('/')} src={assets.logo} alt="Logo"className="w-28 lg:w-32 cursor-pointer "/>
-      <div className="hidden md:flex item-center gap-5 text-gap-5 text-gray-500">
+      <div className="hidden md:flex items-center gap-5 text-gray-500">
         <div className="flex items-center gap-5">
           {user &&
           <>
-          <button> Bccome Educator</button>
+          <button onClick={()=> navigate('/educator')}>{isEducator ?'Educator Dashboard' : 'Become Educator'}</button>
           <Link to ="/my-enrollments">My Enrollments</Link>
-          </>}
+          </>
+          }
         </div>
-
        { user ? <UserButton/>:
-
         <button onClick={()=> openSignIn()} className="bg-blue-600 text-white px-5 py-2 rounded-full">
           Create Account
         </button>}
       </div>
       {/* For phone view?? */}
-      <div className="m d:hidden flex items-center gap-2 sm:gap-5 taxt-gray-500">
+      <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
         <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
           {user &&
           <>
-          <button> Bccome Educator</button>
+          <button onClick={()=> navigate('/educator')}>{isEducator ?'Educator Dashboard' : 'Become Educator'}</button>
           <Link to ="/my-enrollments">My Enrollments</Link>
           </>}
-
         </div>
         {
           user ? <UserButton/>:
           <button onClick={()=> openSignIn()}>
           <img src={assets.user_icon} alt="" />
         </button>
-          }
-        
+          }     
       </div>
     </div>
   );
