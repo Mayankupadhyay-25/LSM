1ed2143c160b86f59776e273a0647842f7894af6
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-12-26 01:49:46 +0530

    TestimonialCompleate

--- src/Components/student/TestimonialSection.jsx
+++ src/Components/student/TestimonialSection.jsx
@@ -3,38 +3,37 @@ import { assets, dummyTestimonial } from '../../assets/assets'
 
 const TestimonialSection = () => {
   return (
-    <div className='pb-14 px-8 md:px-0'>
+    <div className='pb-14 px-5 md:px-0'>
       <h2 className='text-3xl font-medium text-gray-800'>Testimonial</h2>
-      <p>
+      <p className='md:text-base text-gray-500 mt-3'>
         Here from our learners as they are share their journey of transformation, success, and how our
         <br /> platform made a difference in their lives
       </p>
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-14'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10 py-16 md:px-40 px-39'>
         {dummyTestimonial.map((testimonial, index) => (
           <div
             key={index}
-            className='text-sm text-left border border-gray-500/30 pb-6 rounded-lg bg-white shadow-md shadow-black/5 overflow-hidden'
+            className='text-sm text-left border border-gray-500/30 pb-6 rounded-lg bg-white shadow-md
+             shadow-black/5 overflow-hidden'
           >
-            <div className='flex items-center gap-4 px-5 py-4 bg-gray-500/10'>
+            <div className='flex items-center gap-5 px-5 py-4 bg-gray-500/10'>
               <img className='h-12 w-12 rounded-full' src={testimonial.image} alt={testimonial.name} />
               <div>
                 <h1 className='text-lg font-medium text-gray-800'>{testimonial.name}</h1>
                 <p className='text-gray-800/80'>{testimonial.role}</p>
               </div>
             </div>
+
             <div className='p-5 pb-7'>
               <div className='flex gap-0.5'>
                 {[...Array(5)].map((_, i) => (
-                  <img
-                    className='h-5'
-                    key={i}
-                    src={i < Math.floor(testimonial.rating) ? assets.star : assets.star_blank}
-                    alt=''
-                  />
+                  <img className='h-5'key={i}
+                    src={i < Math.floor(testimonial.rating) ? assets.star : assets.star_blank} alt=''/>
                 ))}
               </div>
               <p className='text-gray-500 mt-5'>{testimonial.feedback}</p>
             </div>
+            <a href="#" className='text-blue-500 underline px-5'>Read more</a>
           </div>
         ))}
       </div>
