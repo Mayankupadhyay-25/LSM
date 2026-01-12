8e5b0f0c742e609b31c0cb56d648556ef318938b
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-01-12 21:09:41 +0530

    inProgress

--- src/pages/student/MyEnrollments.jsx
+++ src/pages/student/MyEnrollments.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const MyEnrollments = () => {
   return (
+    
     <div>
-        <h1>MyEnrollments</h1>
+      <h1>My Enrollments</h1>
+      <table>
+        <thead>
+          <tr>
+            <th className="px-4 py-3 font-semibold truncate">Course</th>
+            <th className="px-4 py-3 font-semibold truncate">Duration</th>
+            <th className="px-4 py-3 font-semibold truncate">Completed</th>
+            <th className="px-4 py-3 font-semibold truncate">Status</th>
+          </tr>
+        </thead>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default MyEnrollments
+export default MyEnrollments;
